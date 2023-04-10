8ebe9f996ec44b26c7fbc7e0191f6bb77493f656
Austin Moore <amoore1337@gmail.com>
2023-04-10 08:33:13 -0400

    description update

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,4 +1,5 @@
 import { Link } from "@remix-run/react";
+import clsx from "clsx";
 import type { ReactNode } from "react";
 
 export default function Index() {
@@ -12,17 +13,17 @@ export default function Index() {
       </p>
 
       <p className="w-full max-w-[800px] pb-4">
-        Well, <Code>npm audit</Code> is kinda{" "}
+        Well, <Code>npm audit</Code> is{" "}
         <a
           className="text-sky-600 underline"
           href="https://overreacted.io/npm-audit-broken-by-design/"
           target="_blank"
           rel="noreferrer"
         >
-          broken
+          debatably useless
         </a>
-        .<Code>npm outdated</Code> is a useful tool but I found myself repeating
-        the same steps every time I update dependencies:
+        .<Code>npm outdated</Code> is a useful tool (when it works) but I found
+        myself repeating the same steps every time I update dependencies:
       </p>
 
       <ol className="w-full max-w-[800px] list-decimal pb-4 pl-10">
@@ -41,24 +42,29 @@ export default function Index() {
       <p className="w-full max-w-[800px] pb-4">
         This is a simple utility to assit this repetitious proceedure. Just
         paste in any package.json file to generate a filterable report of all of
-        your project's dependencies. No data is stored about your JSON file. In
-        fact, if you're feeling paranoid, simply provide a valid JSON object
-        with fields for "dependencies" and/or "devDependencies". That's all
-        that's needed.
+        your project's dependencies. No identifying information is stored about
+        your JSON file. In fact, if you're feeling paranoid, simply provide a
+        valid JSON object with fields for "dependencies" and/or
+        "devDependencies". That's all that's needed.
       </p>
 
       <p className="w-full max-w-[800px] pb-4">
         The generated report highlights packages needing to be updated, provides
-        quick links to the package's homepage and repository, and builds update
-        commands based on your selected packages.
+        quick links to the package's npm page, and builds update commands based
+        on your selected packages.
       </p>
 
-      <p className="font-bold">
-        Sound useful for your workflow?{" "}
-        <Link className="italic text-sky-600 underline" to="/audit">
-          Give it a try!
-        </Link>
-      </p>
+      <p className="font-bold">Sound useful for your workflow? </p>
+      <Link
+        className={clsx(
+          "mt-4 rounded border border-solid",
+          "border-green-500 bg-green-500 px-3 py-1 text-white",
+          "hover:border-green-600 hover:bg-green-600"
+        )}
+        to="/audit"
+      >
+        Give it a try!
+      </Link>
     </main>
   );
 }
